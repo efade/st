55d2b35e9c0c474b3696f22e20127c0461a7a5ae
Fakhruddin Ahmad Darwis <fakhri.satu@gmail.com>
2019-12-28 05:17:13 +0700

    Added new keybinding for zooming in and out

--- config.h
+++ config.h
@@ -178,6 +178,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ TERMMOD,              XK_N,           zoom,           {.f = +1} },
+	{ TERMMOD,              XK_E,           zoom,           {.f = -1} },
 };
 
 /*
